c9a25297f97ae8a249c620bf25608f1e3058bf11
snehja17 <132792900+snehja17@users.noreply.github.com>
2023-07-03 11:49:42 +0530

    Add files via upload

---
+++ Untitled35.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import pandas as pd
+
+
+# In[33]:
+
+
+df=pd.read_csv(r'C:\Users\Snehja Srivastava\Downloads\Sorted by Subject Code.csv', header=None, names=['a','b','c'],skiprows=[2,3,4,5])
+
+
+# In[31]:
+
+
+df
+
+
+# In[37]:
+
+
+df=pd.read_csv(r'C:\Users\Snehja Srivastava\Downloads\Sorted by Subject Code.csv', na_values='Roll No.')
+
+
+# In[38]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
